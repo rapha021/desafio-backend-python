136857d31d90f24c1f201c5e2ee7b7eb391ef631
Raphael Gloria <rapha.gloria@gmail.com>
2023-02-06 17:49:56 -0300

    fix: allowed hosts

--- server/_core/settings.py
+++ server/_core/settings.py
@@ -30,10 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "https://cnab-rei-delas-bullet.onrender.com",
-    "https://cnab.vercel.app/",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
