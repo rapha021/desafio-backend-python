7caa86995390d3173f5184933fa9c58f54e1fee9
Raphael Gloria <rapha.gloria@gmail.com>
2023-02-06 03:34:32 -0300

    fix: requirements

--- web/src/services/axios.ts
+++ web/src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  timeout: 50000000,
+  baseURL: "https://cnab-rei-delas-bullet.onrender.com",
+  timeout: 10000,
 })
